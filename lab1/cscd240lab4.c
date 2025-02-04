696f569d97f0de2efd6f636960f2467ec5cc5323
Greg P <88697977+v-9x@users.noreply.github.com>
2025-02-03 20:45:18 -0800

    Finish one function for lab

--- lab1/cscd240lab4.c
+++ lab1/cscd240lab4.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printOddEvenZeros(int n);
 
 int main(void) {
   int MENU_OPTION;
   int n;
   do {
-    printf("--------------");
-    printf("1. Enter a new positive integer.");
-    printf("2. Print the number of odd digits, even digits, and zeros in the "
-           "integer.");
-    printf("3. Print the prime numbers between 2 and the integer (inclusive)");
-    printf("4. Quit the program.");
-    printf("--------------");
-    printf("Please select an option 1 -4.");
+    printf("--------------\n");
+    printf("1. Enter a new positive integer.\n");
+    printf(
+        "2. Print the number of odd digits, even digits, and zeros in the "
+        "integer.\n");
+    printf(
+        "3. Print the prime numbers between 2 and the integer (inclusive)\n");
+    printf("4. Quit the program.\n");
+    printf("--------------\n");
+    printf("Please select an option 1 -4.\n");
     scanf("%d", &MENU_OPTION);
-
     switch (MENU_OPTION) {
-    case 1:
-      scanf("%d", &n);
-      break;
+      case 1:
+        printf("Please enter an integer: ");
+        scanf("%d", &n);
+        break;
+      case 2:
+        printOddEvenZeros(n);
+        break;
     }
   } while (MENU_OPTION != 4);
+}
+
+void printOddEvenZeros(int n) {
+  int e = 0;
+  int o = 0;
+  int z = 0;
+  char* nString;
+  sprintf(nString, "%d", n);  // store number as string in nString
+  int length = strlen(nString);
+
+  for (int i = 0; i < length; i++) {
+    if ((nString[i] - '0') % 2 == 0) {
+      e++;
+    } else if ((nString[i] - '0') % 2 != 0) {
+      o++;
+    } else {
+      z++;
+    }
+  }
+  printf("The number %d has %d even numbers, %d odd numbers, and %d zeros.\n",
+         n, e, o, z);
 }
