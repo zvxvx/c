0c9f2d399e12b187fdc71ff9794ebb037190e4ea
Greg P <88697977+v-9x@users.noreply.github.com>
2025-02-27 17:40:34 -0800

    Files

--- lab7-files_dynamic_arrays/lab7.c
+++ lab7-files_dynamic_arrays/lab7.c
@@ -1,52 +1,47 @@
 #include "lab7.h"
+#include <stdio.h>
 
 extern const int MAX;
 
-int menu()
-{
-	int choice;
-	do
-	{
-		printf("Please enter a menu choice\n");
-		printf("1) Display Max Temp\n");
-		printf("2) Display Min Temp\n");
-		printf("3) Display Average Temp\n");
-		printf("4) Display Median Temp\n");
-		printf("5) Fill an array by reading from a new file\n");
-		printf("6) Quit\n");
-		printf("Choice --> ");
-		scanf("%d", &choice);
-	}	while(choice < 1 || choice > 6);
+int menu() {
+  int choice;
+  do {
+    printf("Please enter a menu choice\n");
+    printf("1) Display Max Temp\n");
+    printf("2) Display Min Temp\n");
+    printf("3) Display Average Temp\n");
+    printf("4) Display Median Temp\n");
+    printf("5) Fill an array by reading from a new file\n");
+    printf("6) Quit\n");
+    printf("Choice --> ");
+    scanf("%d", &choice);
+  } while (choice < 1 || choice > 6);
 
-	return choice;
+  return choice;
 }
 
-int readDays(FILE * fin)
-{
-	return 0;
-}
-int * fillArray(int total, FILE * fin)
-{
-	int * temp = NULL;
-	
-	
-	return temp;
-
+int readDays(FILE *fin) {
+  int days;
+  return fscanf(fin, "%d", &days);
 }
 
-void displayMaxTemp(int * array, int total){}
-void displayMinTemp(int * array, int total){}
-void displayAvgTemp(int * array, int total){}
-void displayMedianTemp(int * array, int total){}
-void cleanUp(int * array){}
+int *fillArray(int total, FILE *fin) {
+  int *temp = NULL;
 
-void printArray(int * array, int total)
-{
-	int x;
-	printf("[");
-	for(x = 0; x < total - 1; x++)
-		printf("%d, ", array[x]);
-
-	printf("%d]\n", array[total - 1]);
+  return temp;
 }
 
+void displayMaxTemp(int *array, int total) {}
+void displayMinTemp(int *array, int total) {}
+void displayAvgTemp(int *array, int total) {}
+void displayMedianTemp(int *array, int total) {}
+void cleanUp(int *array) {}
+
+void printArray(int *array, int total) {
+  int x;
+  printf("[");
+  for (x = 0; x < total - 1; x++)
+    printf("%d, ", array[x]);
+
+  printf("%d]\n", array[total - 1]);
+}
--- lab7-files_dynamic_arrays/utils/fileUtils.c
+++ lab7-files_dynamic_arrays/utils/fileUtils.c
@@ -1,39 +1,36 @@
 #include "fileUtils.h"
+#include <stdio.h>
 
 const int MAXIMUM = 100;
 
-void readFileName(char * fn)
+void readFileName(char *fn) {
+  printf("Please enter the name of the input file ");
+  fgets(fn, MAXIMUM, stdin);
+  strip(fn);
+
+} // end readFileName
+
+FILE *openInputFile(char *fn) // will ensure the file is opened. If not reprompt
+                              // by calling readFileName
 {
-	printf("Please enter the name of the input file ");
-	fgets(fn, MAXIMUM, stdin);
-	strip(fn);
-	
-}// end readFileName
+  FILE *fin = NULL;
+  fin = fopen(fn, "r");
+  return fin;
 
+} // end openInputFile
 
-FILE * openInputFile(char * fn) // will ensure the file is opened. If not reprompt by calling readFileName
-{
-	FILE * fin = NULL;
-	return fin;
-	
-}// end openInputFile
+void strip(char *array) {
+  int len = strlen(array);
+  int x = 0;
 
+  while (x < len) {
+    if (array[x] == '\r')
+      array[x] = '\0';
 
-void strip(char * array)
-{
-	int len = strlen(array);
-	
-	int x = 0;
-	
-	while(x < len)
-	{
-		if(array[x] == '\r')
-			array[x] = '\0';
-			
-		else if(array[x] == '\n')
-			array[x] = '\0';
-			
-		x++;
-	}// end while
+    else if (array[x] == '\n')
+      array[x] = '\0';
 
-}// end strip
+    x++;
+  } // end while
+
+} // end strip
