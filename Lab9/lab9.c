a8391282c775852c203f60991f0e802e5098b1d4
Greg P <88697977+v-9x@users.noreply.github.com>
2025-03-10 17:38:12 -0700

    log

--- Lab9/lab9.c
+++ Lab9/lab9.c
@@ -9,62 +9,50 @@ struct Stock {
 	double currentPrice;
 };
 
-int menu()
-{
-   int num;
-   do
-   {
-   	printf("Please choose from the following\n");
-   	printf("1) Sort by Symbol\n");
-   	printf("2) Sort by Company Name\n");
-   	printf("3) Sort by Price\n");
-   	printf("4) Quit\n");
-   	printf("Choice --> ");
-   	scanf("%d", &num);
-	   while(fgetc(stdin) != '\n');
-   }while(num < 1 || num > 4);
+int menu() {
+	int num;
+	do {
+		printf("Please choose from the following\n");
+		printf("1) Sort by Symbol\n");
+		printf("2) Sort by Company Name\n");
+		printf("3) Sort by Price\n");
+		printf("4) Quit\n");
+		printf("Choice --> ");
+		scanf("%d", &num);
+		while (fgetc(stdin) != '\n');
+	} while (num < 1 || num > 4);
 
-   return num;
-}// end menu
+	return num;
+} // end menu
 
 
-Stock * fillArray(FILE * fin, int total)
-{
-   int x = 0;
-   char temp[100];
-   Stock * array = NULL;
-   return array;
+Stock *fillArray(FILE *fin, int total) {
+	int x = 0;
+	char temp[100];
+	Stock *array = NULL;
+	return array;
 }
 
 
-void symbolSort(Stock array[], int total)
-{
-   
+void symbolSort(Stock array[], int total) {
 }
 
 
-void printArray(Stock array[], int total)
-{
-   int x;
-   for(x = 0; x < total; x++)
-      printf("%s - %s - $%.2lf\n\n", array[x].name, array[x].symbol, array[x].price);
-
+void printArray(Stock array[], int total) {
+	int x;
+	for (x = 0; x < total; x++)
+		printf("%s - %s - $%.2lf\n\n", array[x].name, array[x].symbol,
+		       array[x].price);
 }
 
 
-void companySort(Stock array[], int total)
-{
-   
-}// end companySort
+void companySort(Stock array[], int total) {
+} // end companySort
 
 
-void priceSort(int total, Stock array[])
-{
-   
-}// end priceSort
+void priceSort(int total, Stock array[]) {
+} // end priceSort
 
 
-void cleanUp(Stock * array, int total)
-{
-   
-}// end cleanUp
+void cleanUp(Stock *array, int total) {
+} // end cleanUp
--- Lab9/utils/fileUtils.c
+++ Lab9/utils/fileUtils.c
@@ -1,35 +1,28 @@
 #include "fileUtils.h"
 #include "utils.h"
 
-void readFileName(char * fn)
-{
+void readFileName(char *fn) {
     printf("Please enter the name of the file ");
     fgets(fn, MAXIMUM, stdin);
     strip(fn);
+} // end readFileName
 
-}// end readFileName
-
-FILE * openInputFile()
-{
+FILE *openInputFile() {
     char fn[MAXIMUM];
-    FILE * inf =NULL;
-    do
-    {
+    FILE *inf = NULL;
+    do {
         readFileName((fn));
         inf = fopen(fn, "r");
-
-    }while(inf == NULL);
+    } while (inf == NULL);
 
     return inf;
 }
 
-int countRecords(FILE * fin, const int linesPerRecord)
-{
+int countRecords(FILE *fin, const int linesPerRecord) {
     int count = 0;
     char temp[MAXIMUM];
 
+
     return count / linesPerRecord;
-
-
 }
 
